28b27f832b9e305518a3fd7a99d627e9c9e8b98c
WYS <20211120042@mail.ynu.edu.cn>
2025-02-04 23:27:02 +0800

    修改数据集地址

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -4,13 +4,13 @@ NUM_GPUS=1
 
 torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --do_train \
-    --train_file AdvertiseGen/train.json \
-    --validation_file AdvertiseGen/dev.json \
+    --train_file data/train.json \
+    --validation_file data/dev.json \
     --preprocessing_num_workers 10 \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
-    --model_name_or_path THUDM/chatglm2-6b \
+    --model_name_or_path /autodl-fs/data/ChatGLM \
     --output_dir output/adgen-chatglm2-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
     --max_source_length 64 \
