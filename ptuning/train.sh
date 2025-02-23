28d3e1d3a41d1773550d5a9092aeab9def54c04b
YushanWu23 <20211120042@mail.ynu.edu.cn>
2025-02-23 23:11:19 +0800

    调整参数，减短步数，减少运行时间

--- ptuning/train.sh
+++ ptuning/train.sh
@@ -6,7 +6,7 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --do_train \
     --train_file data/train_new.json \
     --validation_file data/val_new.json \
-    --preprocessing_num_workers 10 \
+    --preprocessing_num_workers 20 \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
@@ -16,20 +16,18 @@ torchrun --standalone --nnodes=1 --nproc-per-node=$NUM_GPUS main.py \
     --overwrite_output_dir \
     --max_source_length 300 \
     --max_target_length 1024 \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
-    --gradient_accumulation_steps 8 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 4 \
     --predict_with_generate \
-    --max_steps 10000 \
-    --logging_steps 10 \
-    --save_steps 1000 \
+    --max_steps 5000 \
+    --logging_steps 50 \
+    --save_steps 2000 \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN \
     --quantization_bit 4 \
-    --num_train_epochs 50 \
-    --evaluation_strategy epoch \
-    --save_strategy epoch \
     --weight_decay 0.005 \
     --dropout_rate 0.05 \
     --lr_scheduler_type cosine \
-    --warmup_steps 500 \
+    --warmup_steps 200 \
+    --fp16 \
